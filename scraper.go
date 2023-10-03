71c7e1e16a00c59209efc85c67e0277e2e0d21bb
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2023-10-03 08:37:51 +0530

    Added posts schema and updated scraper to create post records

--- scraper.go
+++ scraper.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"log"
+	"strings"
 	"sync"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/thejasmeetsingh/rss-aggregator/internal/database"
 )
 
@@ -46,7 +49,37 @@ func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
 	}
 
 	for _, item := range rssFeed.Channel.Item {
-		log.Println("Post found:", item.Title, "on feed", feed.Name)
+		description := sql.NullString{}
+
+		if item.Description != "" {
+			description.String = item.Description
+			description.Valid = true
+		}
+
+		pubDate, err := time.Parse(time.RFC1123Z, item.PubDate)
+
+		if err != nil {
+			log.Printf("Error while parsing date %v with error %v", item.PubDate, err)
+			continue
+		}
+
+		_, err = db.CreatePost(context.Background(), database.CreatePostParams{
+			ID:          uuid.New(),
+			CreatedAt:   time.Now().UTC(),
+			ModifiedAt:  time.Now().UTC(),
+			Title:       item.Title,
+			Url:         item.Link,
+			Description: description,
+			PublishedAt: pubDate,
+			FeedID:      feed.ID,
+		})
+		if err != nil {
+			if strings.Contains(err.Error(), "duplicate key") {
+				continue
+			}
+			log.Println("Error while creating post: ", err)
+			continue
+		}
 	}
 	log.Printf("Feeds %s fetched, Total number of post found %d", feed.Name, len(rssFeed.Channel.Item))
 }
